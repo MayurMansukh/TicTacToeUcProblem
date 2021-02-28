9ce6014d7d82182258c872b8ae0b465fb97ecff8
MayurMansukh <Mansukhmayur99@gmail.com>
2021-02-28 22:37:17 +0530

    create board

---
+++ TicTacToe.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class TicTacToe {
+	public static char[] board;
+	
+	static char[] createBoard() {
+
+		char[] board = new char[10];
+		
+		for(int i = 1; i < 10; i++) {
+			board[i] = ' ';
+		}
+		return board;
+	
+
+	
+	public static void main(String[] args) {
+		System.out.println("Welocme to Tic-Tac-Toe program");
+		board = new char[10];
+		
+		
+}
