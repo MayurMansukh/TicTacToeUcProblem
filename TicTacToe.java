841ba842737f63d155ad90d2dab44bc0c671df5f
MayurMansukh <Mansukhmayur99@gmail.com>
2021-03-02 19:51:01 +0530

    check win or tie

--- TicTacToe.java
+++ TicTacToe.java
@@ -5,6 +5,33 @@ public class TicTacToe {
 	public static String PlayerLetter;
 	public static String ComputerLetter;
 	static Scanner sc = new Scanner(System.in);
+	static int count;
+	static int chance;
+
+	static ArrayList<int[]> checkCondition = new ArrayList<>();
+
+	public static void conditionsForWin()
+	{
+		int[] row1 = {1, 2, 3};
+		int[] row2 = {4, 5, 6};
+		int[] row3 = {7, 8, 9};
+		int[] col1 = {1, 4, 7};
+		int[] col2 = {2, 5, 8};
+		int[] col3 = {3, 6, 9};
+		int[] diag1 = {1, 5, 9};
+		int[] diag2 = {3, 5, 7};
+
+		checkCondition.add(row1);
+		checkCondition.add(row2);
+		checkCondition.add(row3);
+		checkCondition.add(col1);
+		checkCondition.add(col2);
+		checkCondition.add(col3);
+		checkCondition.add(diag1);
+		checkCondition.add(diag2);
+	}
+
+
 	
 	static char[] createBoard() {
 
@@ -89,15 +116,88 @@ public class TicTacToe {
 		
 		if(opt == toss) {
 			System.out.println("Player won the toss! So player starts the game.");
-			makePlayerMove();
-			makeComputerMove();
+			count = 0;
 		}
 		else {
 			System.out.println("Computer won the toss! So computer starts the game.");
-			makeComputerMove();
-			makePlayerMove();
+			count = 1;
 		}
 		
+	}
+public static char winCheck()
+	{
+		final int WIN_CONDITION = 3;
+
+		for (int index = 0; index < checkCondition.size(); index++)
+		{
+			int addX = 0;
+			int addO = 0;
+			for (int i = 0; i < checkCondition.get(index).length; i++)
+			{
+				if (board[checkCondition.get(index)[i]] == 'X')
+				{
+					addX += 1;
+					if (addX == WIN_CONDITION)
+					{
+						return 'X';
+					}
+				}
+				else if (board[checkCondition.get(index)[i]] == 'O')
+				{
+					addO += 1;
+					if (addO == WIN_CONDITION)
+					{
+						return 'O';
+					}
+				}
+			}
+		}
+		return ' ';
+	}
+
+	public static boolean tie()
+	{
+		for (int k = 1; k < 10; k++)
+		{
+			if (board[k] == ' ')
+			{
+				return false;
+			}
+		}
+		return true;
+	}
+
+	public static void turn()
+	{
+		while (true)
+		{
+			if (winCheck() == 'X' || winCheck() == 'O')
+			{
+				System.out.println((userLetter == winCheck()) ? "You Win" : "Computer Wins");
+				break;
+			}
+			else if (tie())
+			{
+				System.out.println("It's a tie");
+				break;
+			}
+			else
+			{
+				if (count%2 == chance)
+				{
+					System.out.println("Your Turn");
+					makeMove();
+					showBoard();
+				}
+				else
+				{
+					System.out.println("Computer Turn");
+					computerTurn();
+					showBoard();
+				}
+				count++;
+			}
+		}
 	}
 
         public static void PlayAgain(){
@@ -122,7 +222,7 @@ public class TicTacToe {
 		System.out.println("Computer is : " + ComputerLetter);
 		showBoard();
 		Toss();
-		
+		turn();
 		
 	}
 }
